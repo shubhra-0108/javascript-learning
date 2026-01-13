f83f6227ba4b729d6f1b42e7051793d279bdb5aa
Shubhra Pandey <shubhrapandey127@gmail.com>
2026-01-13 04:19:41 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) we are using nodejs not browser
+// code readability should be high
+
+let name = "shubhra"
+let age = 20
+let state = null
+let isloggedin = false
+
+/*
+number =>2 to power 53
+bigint 
+string => " "
+boolean => true/false
+null = standalone value
+undefined
+symbol => uniqueness
+*/ 
+
+// object
+
+console.log(typeof shubhra)
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
